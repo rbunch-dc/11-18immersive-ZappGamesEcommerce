d3d3b5040fe715e9c311612be3df3e17f2a3937d
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-02-26 14:36:09 -0500

    Add pg stuff

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const passport = require('passport');
-
+const pgp = require('pg-promise')();
+const config = require('../config');
+const connection  = config.pg;
+const db = pgp(connection);
 router.get('/auth/github',passport.authenticate('github'));
 
 router.get('/auth/github/callback',passport.authenticate('github'),(req,res)=>{
-  res.json(req.user);
+  const selectQuery = `SELECT * FROM users`;
+  const pgPromise = db.query(selectQuery);
+  // console.log(pgPromise);
+  pgPromise.then((data)=>{
+    console.log(data);
+    res.json(data);
+  })
+  // res.json(req.user);
 })
 
 module.exports = router;
