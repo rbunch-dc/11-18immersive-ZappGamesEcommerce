2e776cbb8e1d1da5c00183e168bf132b69d5e820
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-02-28 14:22:01 -0500

    finish game card formatting

--- front-end/src/components/utility/GameCard.js
+++ front-end/src/components/utility/GameCard.js
@@ -12,12 +12,12 @@ function GameCard(props){
             <img className="activator" src={image} />
             </div>
             <div className="card-content">
-            <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-            <p><a href="#">This is a link</a></p>
+            <span className="card-title activator grey-text text-darken-4">{props.data.name}<i className="material-icons right">more_vert</i></span>
+            <p><a href={props.data.url}>Game homepage</a></p>
             </div>
             <div className="card-reveal">
-            <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            <span className="card-title grey-text text-darken-4">{props.data.name}<i className="material-icons right">close</i></span>
+            <p>{props.data.summary}</p>
             </div>
         </div>        
     </div>
