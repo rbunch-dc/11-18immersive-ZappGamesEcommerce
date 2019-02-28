403a95232cc6c4d755436601ce67f6f78f04065f
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-02-28 12:04:55 -0500

    Add login

--- front-end/src/components/pages/Login.js
+++ front-end/src/components/pages/Login.js
@@ -3,11 +3,38 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import loginAction from '../../actions/loginAction';
+import SweetAlert from 'sweetalert-react';
+import 'sweetalert/dist/sweetalert.css'
 
 class Login extends Component{
-
+    constructor(){
+        super();
+        this.state = {
+            showAlert: false,
+            msg: "",
+        }
+    }
     componentWillReceiveProps(newProps){
         console.log(newProps);
+        if(newProps.auth.msg === 'badPassword'){
+            // let the user know they already registered
+            this.setState({
+                showAlert: true,
+                msg: "Please re-enter your password"
+            })
+        }else if(newProps.auth.msg === 'badUser'){
+            this.setState({
+                showAlert: true,
+                msg: "Username not found"
+            })
+        }else if(newProps.auth.msg === 'loginSuccess'){
+            this.props.history.push('/');
+        }
+
+    }
+
+    componentDidMount(){
+        
     }
 
     handleLogin = (event)=>{
@@ -23,6 +50,13 @@ class Login extends Component{
     render(){
         return(
         <main>
+            <SweetAlert
+                show={this.state.showAlert}
+                title="Login Error"
+                text={this.state.msg}
+                onConfirm={() => this.setState({ showAlert: false })}
+            />            
+
             <center>
             <div className="container">
                 <div className="z-depth-1 grey lighten-4 row login">
