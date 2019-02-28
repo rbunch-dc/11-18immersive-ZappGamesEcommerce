7722b04381b8d75fe080ffc988326167dcde1348
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-02-28 15:39:11 -0500

    Add cart stuff

--- back-end/routes/games.js
+++ back-end/routes/games.js
@@ -6,14 +6,24 @@ const db = require('../database');
 // /games/getHome
 router.get('/getHome',(req, res)=>{
     const gameQuery = `SELECT * FROM games
-        WHERE screenshot_url IS NOT NULL 
-        ORDER BY popularity desc 
+        WHERE screenshot_url IS NOT NULL
+        ORDER BY RANDOM() desc 
         limit 4;
     `;
     db.query(gameQuery).then((results)=>{
         res.json(results)
     }).catch((error)=>{
         if(error){throw error}
+    })
+})
+
+router.get('/:gid',(req, res)=>{
+    const gid = req.params.gid;
+    const selectQuery = `SELECT * FROM games WHERE id = $1;`;
+    db.query(selectQuery,[gid]).then((gameData)=>{
+        res.json(gameData);
+    }).catch((error)=>{
+        if(error){throw error;}
     })
 })
 
--- front-end/src/components/utility/GameCard.js
+++ front-end/src/components/utility/GameCard.js
@@ -1,25 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function GameCard(props){
     console.log(props);
     const images = props.data.screenshot_url.split(',');
     const rand = Math.floor(Math.random() * images.length)
-    const image = images[rand];
+    let image = images[rand];
+    image = image.replace('t_thumb','t_cover_big')
+
     return(
         <div className="col s3 game-card">
-            <div className="card">
-            <div className="card-image waves-effect waves-block waves-light">
-            <img className="activator" src={image} />
-            </div>
-            <div className="card-content">
-            <span className="card-title activator grey-text text-darken-4">{props.data.name}<i className="material-icons right">more_vert</i></span>
-            <p><a href={props.data.url}>Game homepage</a></p>
-            </div>
-            <div className="card-reveal">
-            <span className="card-title grey-text text-darken-4">{props.data.name}<i className="material-icons right">close</i></span>
-            <p>{props.data.summary}</p>
-            </div>
-        </div>        
+            <Link to={`/game/${props.data.id}`}>
+                <div className="card">
+                    <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src={image} />
+                    </div>
+                    <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">{props.data.name}<i className="material-icons right">more_vert</i></span>
+                    </div>
+                </div>        
+            </Link>
     </div>
     )
 }
