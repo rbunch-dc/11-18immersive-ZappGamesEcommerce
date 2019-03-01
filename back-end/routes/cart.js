ec290e229626e4cab1bf0677bab4fecb8eeef933
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-03-01 10:38:54 -0500

    cart update

--- back-end/routes/cart.js
+++ back-end/routes/cart.js
@@ -3,8 +3,21 @@ var router = express.Router();
 const db = require('../database');
 
 // /cart/getCart
-router.get('/getCart',(req, res)=>{
-    res.json("test");
+router.post('/getCart',(req, res)=>{
+    // res.json("test");
+    const token = req.body.token;
+    const getUser = `SELECT id from users WHERE token = $1`
+    db.query(getUser,[token]).then((results)=>{
+        if(results.length === 0){
+            // This is a bad token. User is confused or a liar or a locationvar.
+            res.json({
+                msg: "badToken"
+            })
+        }
+    }).catch((error)=>{
+        if(error){throw error;} 
+    })
+
 })
 
 module.exports = router;
